70be920e2f36da6f20f938492b4f80195fceea97
mehdi <100924042+mehdiEnnabli@users.noreply.github.com>
2022-06-24 11:57:49 +0100

    init server

---
+++ travelcompanion/pages/signup.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import Head from 'next/head'
+
+function signup() {
+  return (
+        <div>
+            <Head>
+                <title>signup</title>
+            </Head>
+            signup
+        </div>
+  )
+}
+
+export default signup
