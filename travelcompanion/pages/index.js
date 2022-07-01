dea1dbbebbc3001e1003ab8b6eeef4810ce62700
mehdi <100924042+mehdiEnnabli@users.noreply.github.com>
2022-07-01 16:06:20 +0100

    navbar

--- travelcompanion/pages/index.js
+++ travelcompanion/pages/index.js
@@ -6,6 +6,7 @@ import AddCity from '../componenents/fireStore/AddCity'
 import AddLine from '../componenents/fireStore/AddLine'
 import ChatRoom from '../componenents/fireStore/chatRoom'
 import Posts from '../componenents/fireStore/Posts'
+import SideBar from '../componenents/SideBar'
 export default function Home() {
   return (
     <div className={styles.container}>
@@ -13,14 +14,27 @@ export default function Home() {
         <title>adminTC</title>
         <meta name='description' content='admin Travel Compagnion ' />
       </Head>
-      <h1>Welcome to Next</h1>
-      <AddStops/>
+      
+      <div style={{
+        position: "absolute",
+        top: "0px",
+        right: "0",
+        width: "87%",
+        height: "100%",
+        
+      }}>
+        <SideBar />
+              <h1 className='text-5xl'>Welcome to Next</h1>
+
+        <AddStops/>
       <AddCity/>
       <AddLine/>
       <ChatRoom/>
       <h2>users</h2>
       <UsersManagment/>
       <Posts/>
+      </div>
+      {/*  */}
     </div>
   )
 }
